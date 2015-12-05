44aa1c61b95b21fe83a0d3dea067913f06a452eb
jlaportetn <jlaporte81@gmail.com>
2015-12-05 10:47:54 -0600

    Added pages 44 and 48

---
+++ Pg44/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine("17/4 is " + quotient + " remainder " + remainder);
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Pg48/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cylinder Calculator!"); //Welcome message
+            Console.WriteLine("Enter the cylinder's radius: "); //Ask for user input
+            string radiusAsAString = Console.ReadLine();  //Create string based on user input
+            double radius = Convert.ToDouble(radiusAsAString);  //Convert the string to a double
+
+            Console.WriteLine("Enter the cylinder's height: ");
+            string heightAsAString = Console.ReadLine();
+            double height = Convert.ToDouble(heightAsAString);
+
+            double pi = 3.14592654;  //Assign value to var pi
+
+            double volume = pi * radius * radius * height;  //Create double using volume formula
+
+            double surfaceArea = 2 * pi * radius * (radius + height);  //Create double using surface area formula
+
+            Console.WriteLine("The cylinder's volume is " + volume + " cubic units.");  //Display volume results to user
+            Console.WriteLine("The cylinder's surface are is " + surfaceArea + " square units.");  //Same for surface A
+
+            Console.ReadKey();  //Wait for user response before closing
+        }
+    }
+}
