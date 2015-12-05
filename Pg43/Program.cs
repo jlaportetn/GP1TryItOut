c6a2dd0739436dd914188fa7271ca9457d305c7e
jlaportetn <jlaporte81@gmail.com>
2015-12-05 10:33:31 -0600

    Added Pg.43

---
+++ Pg43/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float triangleBASE = 5f;
+            float triangleHeight = 6f;
+            float triangleArea = triangleBASE * triangleHeight / 2;
+            Console.WriteLine("The area of the triangle is " + triangleArea + ".");
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
